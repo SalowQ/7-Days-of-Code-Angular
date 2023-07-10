0584dde7bb03f9e6b51f61e9a69433ee7b900c1f
igor <arqurb.igor@gmail.com>
2023-07-10 15:07:59 -0300

    lista de filmes e estilizações

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,21 +1,59 @@
 import { Movie } from 'src/app/movie';
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.sass'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'),
+      ),
+    ]),
+  ],
 })
 export class MoviesComponent implements OnInit {
-  movie?: Movie;
+  movies?: Movie[];
+
+  dataSource = MOVIES_DATA;
+  columnsToDisplay = [
+    'episode_id',
+    'title',
+    'director',
+    'producer',
+    'release_date',
+  ];
+  columnNames: { [key: string]: string } = {
+    episode_id: 'Episode',
+    title: 'Title',
+    director: 'Director',
+    producer: 'Producer',
+    release_date: 'Release Date',
+  };
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
+  expandedMovie?: Movie | null;
 
   constructor(private service: ApiService) {}
-  pegarFilmes() {}
 
   ngOnInit() {
     this.service.fetch().subscribe((movies) => {
-      this.movie = movies[0];
+      this.movies = movies;
+      this.dataSource = movies;
+      console.log(this.dataSource);
     });
   }
 }
+
+const MOVIES_DATA: Movie[] = [];
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
-  exports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+  ],
+  exports: [MatToolbarModule, MatIconModule, MatButtonModule, MatTableModule],
 })
 export class MaterialModule {}
